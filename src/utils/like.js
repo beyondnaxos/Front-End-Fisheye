8685a2b18fda66728afc1eb8f5780285d6396b0c
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-08-16 14:51:06 +0200

    Fix Likes OK

--- src/utils/like.js
+++ src/utils/like.js
@@ -3,34 +3,31 @@ class Like {
     this.id = id
     this.heartIcon = document.querySelector(`[data-id="${this.id}"]`)
     this.mediaLikesCount = mediaLikesCount
-    this.liked = false
   }
 
   static init() {
     const heartIcons = document.querySelectorAll('.heart-icon')
-
     heartIcons.forEach((heartIcon) => {
       heartIcon.addEventListener('click', (e) => {
         e.preventDefault()
         const parent = heartIcon.parentElement
         const mediaLikesCount = parent.querySelector('.media-likes-count')
         console.log(heartIcon)
-        Like.like(mediaLikesCount)
+        Like.like(mediaLikesCount, heartIcon)
       })
     })
   }
 
-  static like(mediaLikesCount) {
-    if (this.liked === true) {
-      const likesCount = document.querySelector('.likes-count')
+  static like(mediaLikesCount, heartIcon) {
+    const likesCount = document.querySelector('.likes-count')
+    if (heartIcon.classList.contains('liked')) {
       likesCount.innerHTML = parseInt(likesCount.innerHTML) - 1
       mediaLikesCount.innerHTML = parseInt(mediaLikesCount.innerHTML) - 1
-      this.liked = false
+      heartIcon.classList.remove('liked')
     } else {
-      const likesCount = document.querySelector('.likes-count')
       likesCount.innerHTML = parseInt(likesCount.innerHTML) + 1
       mediaLikesCount.innerHTML = parseInt(mediaLikesCount.innerHTML) + 1
-      this.liked = true
+      heartIcon.classList.add('liked')
     }
   }
 }
