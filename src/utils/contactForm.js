a1bf6dee0e0d92d8c0341d66c0c708638cc8470b
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-08-23 00:37:05 +0200

    Fix Focus Contact OK

--- src/utils/contactForm.js
+++ src/utils/contactForm.js
@@ -28,6 +28,38 @@ function displayModal() {
   const modalTitle = document.querySelector('.modal-title')
   const headingName = document.querySelector('.heading-name').textContent
   modalTitle.innerHTML = `Contactez moi </br> ${headingName}`
+  const focusableElements =
+    'button, input, textarea, [tabindex]:not([tabindex="-1"])'
+  const firstFocusableElement = modal.querySelectorAll(focusableElements)[0] // get first element to be focused inside modal
+  const focusableContent = modal.querySelectorAll(focusableElements)
+  const lastFocusableElement = focusableContent[focusableContent.length - 1] // get last element to be focused inside modal
+  console.log(focusableContent)
+  console.log(modal)
+
+  document.addEventListener('keydown', function (e) {
+    let isTabPressed = e.key === 'Tab'
+
+    if (!isTabPressed) {
+      return
+    }
+
+    if (e.shiftKey) {
+      // if shift key pressed for shift + tab combination
+      if (document.activeElement === firstFocusableElement) {
+        lastFocusableElement.focus() // add focus for the last focusable element
+        e.preventDefault()
+      }
+    } else {
+      // if tab key is pressed
+      if (document.activeElement === lastFocusableElement) {
+        // if focused has reached to last focusable element then focus first focusable element after pressing tab
+        firstFocusableElement.focus() // add focus for the first focusable element
+        e.preventDefault()
+      }
+    }
+  })
+
+  firstFocusableElement.focus()
 }
 
 function closeModal() {
@@ -71,3 +103,35 @@ document.addEventListener(keydownpressed, (e) => {
     closeModal()
   }
 })
+
+// const  focusableElements =
+//     'button, input, textarea, [tabindex]:not([tabindex="-1"])';
+// const modal = document.querySelector('#contact_modal'); // select the modal by it's id
+
+// const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+// const focusableContent = modal.querySelectorAll(focusableElements);
+// const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+// console.log(focusableContent)
+// console.log(modal)
+
+// document.addEventListener('keydown', function(e) {
+//   let isTabPressed = e.key === 'Tab'
+
+//   if (!isTabPressed) {
+//     return;
+//   }
+
+//   if (e.shiftKey) { // if shift key pressed for shift + tab combination
+//     if (document.activeElement === firstFocusableElement) {
+//       lastFocusableElement.focus(); // add focus for the last focusable element
+//       e.preventDefault();
+//     }
+//   } else { // if tab key is pressed
+//     if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+//       firstFocusableElement.focus(); // add focus for the first focusable element
+//       e.preventDefault();
+//     }
+//   }
+// });
+
+// firstFocusableElement.focus();
